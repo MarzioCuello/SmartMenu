4ceb82c091a5a361e69e626473bb5f1307358a1f
young-luis <you@example.com>
2020-02-26 20:53:20 -0300

    pusheanding

--- front/src/app/client/client-routing.module.ts
+++ front/src/app/client/client-routing.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ClientComponent } from './client.component';
+import { FavoritesRestaurantsComponent } from './favorites-restaurants/favorites-restaurants.component';
 
-const routes: Routes = [{ path: '', component: ClientComponent }];
+const routes: Routes = [
+  { 
+    path: '', component: ClientComponent 
+  },
+  {
+    path: 'favorites', component: FavoritesRestaurantsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- front/src/app/client/client.module.ts
+++ front/src/app/client/client.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client.component';
+import { SelectRestaurantComponent } from './select-restaurant/select-restaurant.component';
+import { FavoritesRestaurantsComponent } from './favorites-restaurants/favorites-restaurants.component';
 
 
 @NgModule({
-  declarations: [ClientComponent],
+  declarations: [ClientComponent, SelectRestaurantComponent, FavoritesRestaurantsComponent],
   imports: [
     CommonModule,
     ClientRoutingModule
+  ],
+  exports: [
+    SelectRestaurantComponent
   ]
 })
 export class ClientModule { }
