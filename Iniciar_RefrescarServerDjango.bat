5c1ad90b3313e92cc444e9a1b5f2376b616aefcb
MarzioCuello <marzio.cuello@gmail.com>
2020-02-27 02:13:27 -0300

    Initial Django models and Python venv, with batch scripts to initialize them

---
+++ Iniciar_RefrescarServerDjango.bat
@@ -0,0 +1,1 @@
+cmd /k "cd /d venv\Scripts & activate & cd /d ..\.. & cd /d back\backend & python manage.py makemigrations & python manage.py migrate & python manage.py runserver &cd ..\.. cd /d venv\Scripts & deactivate "
--- back/backend/backend/settings.py
+++ back/backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SmartMenuServer',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
