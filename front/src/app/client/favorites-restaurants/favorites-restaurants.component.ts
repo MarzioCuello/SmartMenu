aa14f4b6bf7acb0f8faa3a6db11073ce3e1995fa
young-luis <you@example.com>
2020-02-26 22:37:53 -0300

    some successes

--- front/src/app/client/favorites-restaurants/favorites-restaurants.component.ts
+++ front/src/app/client/favorites-restaurants/favorites-restaurants.component.ts
@@ -6,8 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites-restaurants.component.scss']
 })
 export class FavoritesRestaurantsComponent implements OnInit {
+  restaurants = [
+    {
+      name: "El Fogón",
+      type: "Parrilla",
+      address: "San José 1080",
+      open: {
+        days: "De lunes a domingos",
+        hours: "De 10 a 24"
+      },
+      logo: "elfogon.png",
+    },
+    {
+      name: "Santo Pecado",
+      type: "Parrilla",
+      address: "Bvr. Artigas 4304",
+      open: {
+        days: "De lunes a domingos",
+        hours: "De 10 a 24"
+      },
+      logo: "santopecado.jpg",
+    },
+    {
+      name: "Cilantro Restaurant",
+      type: "Internacional",
+      address: "Ramón Ortiz 2952",
+      open: {
+        days: "De lunes a domingos",
+        hours: "De 10 a 24"
+      },
+      logo: "cilantro.png",
+    }
+  ]
+  favorites: any;
 
-  constructor() { }
+  constructor() { 
+    //this.favorites = JSON.parse('this.restaurants');
+  }
 
   ngOnInit() {
   }
