528c6e58faa42b73e1bddd12f6ad4f8ea54cde92
Javier <young.javier@gmail.com>
2020-03-07 16:40:06 -0300

    icon fix and small details

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 
   { path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'editor', loadChildren: () => import('./editor/editor.module').then(m => m.EditorModule) },
-  { path: '', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) },
+  { path: 'client', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) },
   { path: 'restaurant', loadChildren: () => import('./restaurant/restaurant.module').then(m => m.RestaurantModule) },
 
 
--- front/src/app/restaurant/reservation-detail/reservation-detail.component.ts
+++ front/src/app/restaurant/reservation-detail/reservation-detail.component.ts
@@ -22,5 +22,6 @@ export class ReservationDetailComponent implements OnInit {
 
   ngOnInit() {
   }
+  
 
 }
