28cc16316d5b694dcdab378a38f59da49cac7241
MarzioCuello <marzio.cuello@gmail.com>
2020-03-08 20:50:14 -0300

    Script update 2.0

--- Iniciar_RefrescarServerDjango.sh
+++ Iniciar_RefrescarServerDjango.sh
@@ -1,6 +1,8 @@
 source venv/bin/activate
 cd back/backend
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3 manage.py makemigrations SmartMenuServer
+python3 manage.py makemigrations 
+python3 manage.py migrate SmartMenuServer
+python3 manage.py migrate 
 python3 manage.py runserver
 deactivate
--- InstalarVenv_Django_DRF.sh
+++ InstalarVenv_Django_DRF.sh
@@ -2,6 +2,8 @@ python3 -m venv venv && source venv/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 cd back/backend
-python3 manage.py makemigrations
+python3 manage.py makemigrations SmartMenuServer
+python3 manage.py makemigrations 
+python3 manage.py migrate SmartMenuServer
 python3 manage.py migrate
-
+deactivate
